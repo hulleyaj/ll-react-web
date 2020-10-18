f44b6b37c988b269761884ab74ea2a89c3d9d97b
Allen Hulley <hulleyaj@gmail.com>
2020-10-18 14:00:16 -0400

    no clue

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Button } from 'll-pkg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h2>This is a counter</h2>
+      <Button text="lol" />
     </div>
   );
 }
