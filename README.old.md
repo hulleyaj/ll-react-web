0c2a671191092bc3f8c054f23f31e939561874f0
Allen Hulley <hulleyaj@gmail.com>
2020-10-18 11:07:45 -0400

    initial commit

---
+++ README.old.md
@@ -0,0 +1,2 @@
+# ll-react-web
+ 
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>This is a counter</h2>
+    </div>
+  );
+}
+
+export default App;
